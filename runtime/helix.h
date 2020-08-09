7bda37f7b5fd9b4be78e6d8d69d9bc94c1cf9cfe
Martijn Bakker <gladdyu@gmail.com>
2020-08-09 19:00:06 +0100

    clean up the helix setup to work on linux - tests are todo

--- runtime/helix.h
+++ runtime/helix.h
@@ -2,54 +2,33 @@
 
 #include <cstdint>
 #include <type_traits>
-#include <array>
-#include <thread>
-#include <atomic>
 
 #define ALIGNMENT 64
 
-template<typename T>
-class alignas(ALIGNMENT) MobileAtomic {
-public:
-  MobileAtomic() {}  
-  MobileAtomic(const MobileAtomic& a) : v(a.v.load()) {}
-  
-  MobileAtomic& operator=(const T& x) { v.store(x); return *this; }
-  operator T() {return v.load();}
+class ThreadBuffer;
 
-private:
-  std::atomic<T> v;
-};
+extern "C"
+{
+  struct alignas(ALIGNMENT) ThreadBufferBase
+  {
+    // these values are read by LLVM upon code generation, do not change
+    bool keep_going = true;
+    bool start_prologue = false;
+    uint64_t iteration_to_run;
+    ThreadBuffer *next;
+  };
 
-extern "C" {
+  using optim_type = std::add_pointer<void(ThreadBuffer *, void *)>::type;
+  using opaque_ptr = std::add_pointer<int8_t>::type;
 
-struct alignas(ALIGNMENT) ThreadBuffer {
-// keep this fixed, as LLVM checks
-  bool keep_going = true;
-  bool start_prologue = false;
-  uint64_t iteration_to_run;
-  ThreadBuffer *next;
-  
-  // can add more stuff here
-  ThreadBuffer(int t);
-  int tid;
-  std::thread runner;
-  std::array<MobileAtomic<bool>, 16> flags;
-};
-using optim_type = std::add_pointer<void(ThreadBuffer*,void*)>::type;
-using opaque_ptr = std::add_pointer<int8_t>::type;
+  void thread_signal(ThreadBuffer *next, uint8_t s);
+  void thread_wait(ThreadBuffer *current, uint8_t s);
 
-void thread_signal(ThreadBuffer * next, uint8_t s);
-void thread_wait(ThreadBuffer * current, uint8_t s);
+  void schedule_helix(optim_type o, opaque_ptr carry, int num_threads);
+  void stop_helix(ThreadBuffer *tb);
 
-void schedule_helix(optim_type o, opaque_ptr carry, int num_threads);
-void stop_helix(ThreadBuffer* tb);
-
-void print_abc(int a, int b, int c);
-}
-
-int heavy_function(int x);
-
-void prevent_deletion() {
-	ThreadBuffer x(1);
+  void prevent_deletion()
+  {
+    ThreadBufferBase x;
+  }
 }
